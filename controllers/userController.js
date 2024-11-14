a4f2c3e794cc88140fcad627b3bad3d2ef043177
Eldwin Pradipta <EldwinPradipta670@gmail.com>
2024-11-14 20:36:15 +0700

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,7 +57,7 @@ const loginUser = (req, res) => {
             }
 
             // Generate JWT token
-            const token = jwt.sign({ email: user.email }, 'your_jwt_secret', { expiresIn: '1h' });
+            const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
             res.status(200).json({ message: 'Login successful', token });
         });
     });
